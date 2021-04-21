f7acfd454e1bc76ad6a1b4f0b37a3e05c2c796cd
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-21 20:26:10 +0300

    convinience method to parse and flatten

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import * as E from 'fp-ts/lib/Either';
+import { flow } from 'fp-ts/lib/function';
+
 import * as U from './util';
 
 export const flatten = U.flatten;
 export const parse = U.parse;
+
+/**
+ * Convinience function. Parses JSON `string` into an Either<Err, javascriptObject>
+ * @param src JSON input string
+ * @returns Either<Err, javascriptObject>
+ */
+export const parseJson = flow(parse, E.map(flatten));
 
 export * as P from './parsers';
