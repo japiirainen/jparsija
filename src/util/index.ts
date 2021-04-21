ded65c0986c11dad8ec78651eb84103ad0cff5bf
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-21 20:02:40 +0300

    improve schema

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,14 @@
 import * as E from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/function';
+import * as NEA from 'fp-ts/lib/NonEmptyArray';
+import * as O from 'fp-ts/lib/Option';
+import * as R from 'fp-ts/lib/Record';
+import { pipe, flow, identity } from 'fp-ts/lib/function';
 import { char as C, parser as P, string as S } from 'parser-ts';
 import { ParseError } from 'parser-ts/ParseResult';
 import { stream } from 'parser-ts/Stream';
+
+import { JSON } from '../domain/JSON';
+import { JSONParser } from '../parsers';
 
 export type ExtractParserRight<T> = T extends P.Parser<any, infer R>
    ? R
@@ -86,3 +92,56 @@ export const JSONStringEscapesParser = pipe(
       )
    )
 );
+
+export const flatten = (
+   src: JSON
+): string | number | boolean | null | object => {
+   switch (src._tag) {
+      case 'string':
+         return src.value;
+      case 'number':
+         return src.value;
+      case 'boolean':
+         return src.value;
+      case 'null':
+         return null;
+      case 'object':
+         return pipe(
+            src.value,
+            NEA.fromArray,
+            O.map(
+               flow(
+                  NEA.groupBy(a => a.key),
+                  R.map(NEA.last),
+                  R.map(a => flatten(a.value))
+               )
+            ),
+            O.getOrElse(() => ({}))
+         );
+      case 'array':
+         return src.value.map(flatten);
+   }
+};
+
+export type Err =
+   | {
+        type: 'jsErr';
+        value: unknown;
+     }
+   | {
+        type: 'parseErr';
+        value: ParseError<C.Char>;
+     };
+
+export const parse = (src: string) =>
+   pipe(
+      E.tryCatch(
+         () =>
+            pipe(
+               runParser(JSONParser, src),
+               E.mapLeft((value): Err => ({ type: 'parseErr', value }))
+            ),
+         (value): Err => ({ type: 'jsErr', value })
+      ),
+      E.chain(identity)
+   );
